957b1df6d667569543cad12ad9691503d3c4c051
EmmaKOkyere <okyere.ekakra@gmail.com>
2025-07-17 14:52:32 -0700

    new commit

--- chamber/script/directory.js
+++ chamber/script/directory.js
@@ -1,16 +1,5 @@
-const currentYear = document.querySelector('#currentyear');
-const lastModified = document.querySelector('#lastModified');
-const headerMenu = document.querySelector('.header__menu');
-const navEl = document.querySelector('.header__nav');
-
-const today = new Date();
-const templesLarge = window.matchMedia('(min-width: 425px)');
-
-currentYear.textContent += today.getFullYear();
-lastModified.textContent += document.lastModified;
-
 const mainSectionContainer = document.querySelector(".main__section_container");
-const chambersLink = "../wdd231/chamber/data/members.json";
+const chambersLink = "http://127.0.0.1:5500/wdd231/chamber/data/members.json.preview";
 const gridBtn = document.querySelector(".grid__btn");
 const listBtn = document.querySelector(".list__btn");
 
@@ -60,32 +49,3 @@ function makeItList(container) {
 }
 
 getChambersData();
-
-
-function displayChamberCards(data) {
-    const chamberCardSectionContainer = document.createElement("div");
-    chamberCardSectionContainer.setAttribute('class', 'chamber-card__container');
-
-    data.forEach((object) => {
-        const section = document.createElement("section");
-        section.setAttribute("class", "chamber__card");
-        section.innerHTML = `
-            <h3>${object.name}</h3>
-            <div>
-                <img src='${object.logo}' alt='${object.name} logo' width='80' height='80'>
-                <div class='chamber__card_text'>
-                    <p><span>Address: </span>${object.address}</p>
-                    <p><span>Phone Number: </span>${object.phone}</p>
-                    <p><span>Website: </span><a href='${object.website}'>${object.website}</a></p>
-                    <p><span>Membership: </span>${object.membershipLevel}</p>
-                </div>
-            </div>
-        `;
-        chamberCardSectionContainer.append(section);
-    });
-
-    mainSectionContainer.append(chamberCardSectionContainer);
-    makeItGrid(chamberCardSectionContainer);
-    makeItList(chamberCardSectionContainer);
-}
-
